b61b1f73a2469ac3bed288f89427a6b8f3f329c7
MonteiroFelicianoJoao <57365237+MonteiroFelicianoJoao@users.noreply.github.com>
2020-07-08 01:25:54 -0300

    Entrega

---
+++ Campeoes/Campeoes/CupCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CupCollectionViewCell.swift
+//  Campeoes
+//
+//  Created by aluno on 07/07/20.
+//  Copyright © 2020 CESAR School. All rights reserved.
+//
+
+import UIKit
+
+class CupCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var countryFlag: UIImageView!
+    
+}
